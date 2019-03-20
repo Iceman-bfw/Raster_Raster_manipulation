db0a51050c3504dc2b8e15fe3fe34dba5b1ede9f
Christopher D'Amboise <christopher.damboise@bfw.gv.at>
2019-03-20 15:09:28 +0100

    test commit

--- search_and_destroy.py
+++ search_and_destroy.py
@@ -15,10 +15,12 @@ def read_header(filein):
     #header.append( str(f.readlines(i)[0:6]))  #header + (str(f.readlines(i))[2:-4]) + '\n'
     f.close()
     print(header)
+    return  Header
 
 
 if __name__ == "__main__":
     filein = "/home/damboise/Documents/text_replace/init.asc"
+    header = read_header(filein)
     nodata = int(header.split(' ')[-1])
     #header = "ncols         620\nnrows         412\nxllcenter     61520\nyllcenter     254585\ncellsize      5\nNODATA_value  -9999"
     raster = np.loadtxt(filein, dtype= 'int' ,skiprows=6)
