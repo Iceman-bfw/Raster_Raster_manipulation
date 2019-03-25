91bc48ef7f1a9d5bb63b5a53f2b06d16efce5e3a
Maveric-bfw <Christopher.damboise@gmail.com>
2019-03-25 13:44:01 +0100

    changed .txt output to .asc this is needed for the DakumoTool to make .sh files.

--- empty_raster.py
+++ empty_raster.py
@@ -5,9 +5,12 @@ import numpy as np
 from search_and_destroy import read_header
 
 if __name__ == "__main__":
-    filein = "/home/damboise/Documents/text_replace/init.asc"
+    filein = "/home/damboise/Documents/text_replace/test_dhm_release_.asc"
+    #filein = "/home/damboise/Documents/text_replace/test_dhm_.asc"
     header = read_header(filein)
-    nodata = int(header.split(' ')[-1])
-    raster = np.loadtxt(filein, dtype='int', skiprows=6)
+    # nodata = int(header.split(' ')[-1])
+    raster = np.loadtxt(filein, dtype='float', skiprows=6)
     raster[raster != 0] = int(0)
-    np.savetxt('empty_infra.txt', raster, fmt="%d", header=header, comments='')
+    #raster[raster <= 0.1] = 1
+    np.savetxt("empty_infra.asc", raster, fmt="%d", header=header, comments='')
+    #np.savetxt("non_zero_test_dhm.asc", raster, fmt="%d", header=header, comments='')
--- search_and_destroy.py
+++ search_and_destroy.py
@@ -1,27 +1,25 @@
 import numpy as np
-import os
 
 
 def read_header(filein):
-    f = open(filein, 'r')
-    Header = ''
-    for i in range(6):
-        header = f.readline()
-        Header = Header + header
+    with open(filein, 'r') as f:
+        Header = ''
+        for i in range(6):
+            header = f.readline()
+            Header = Header + header
 
-    nodata = int(header.split(' ')[-1])
-    f.close()
+    # nodata = int(header.split(' ')[-1])
     return Header
 
 
 if __name__ == "__main__":
-    filein = "/home/damboise/Documents/text_replace/init.asc"
+    filein = "/home/damboise/Documents/text_replace/test_dhm_release_.asc"
     header = read_header(filein)
-    nodata = int(header.split(' ')[-1])
-    raster = np.loadtxt(filein, dtype='int', skiprows=6)
+    # nodata = float(int(header.split(' ')[-1]))
+    raster = np.loadtxt(filein,  skiprows=6)
     # change the raster
-    raster[raster == nodata] = -9999
-    raster[raster == int(2)] = int(0)
-    raster[raster == int(3)] = int(0)
+    # raster[raster == nodata] = -9999
+    raster[raster >= 1] = int(1)
+    raster[raster < 1] = int(0)
     # save file
-    np.savetxt('class_1.txt', raster, fmt="%d", header=header, comments='')
+    np.savetxt('class_1.asc', raster, fmt="%d", header=header, comments='')
